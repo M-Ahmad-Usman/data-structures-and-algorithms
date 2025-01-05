fc45eddf88c5593ed9eb4c487b0139148d8df4f4
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-01-06 00:26:19 +0500

    Solve exercise 01 of lab 04

---
+++ lab04/exercise_01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+private:
+    int Data;
+    Node *Next;
+
+public:
+    int GetData()
+    {
+        return Data;
+    }
+
+    Node *GetNext()
+    {
+        return Next;
+    }
+
+    void SetData(int Data)
+    {
+        this->Data = Data;
+    }
+
+    void SetNext(Node *Next)
+    {
+        this->Next = Next;
+    }
+};
+
+int main()
+{
+    return 0;
+}
