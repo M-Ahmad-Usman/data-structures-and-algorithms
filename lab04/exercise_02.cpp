95da43488e95fab71a72fd692aefb0664d4712b2
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-01-06 16:30:26 +0500

    Edit the Delete() in List Class in exercise 02 of lab 04 to handle left out cases. Add a folder in .gitignore

--- lab04/exercise_02.cpp
+++ lab04/exercise_02.cpp
@@ -89,34 +89,48 @@ public:
             return;
         }
 
-        // Case 2: If list is not empty
-        Node *nodeToDelete = currentLocation->GetNext();
-        Node *previous = currentLocation;
-
-        // Case 2.1: If node to be deleted is first node
+         // Case 2.: If first node contains the data
         if (currentLocation->GetData() == dataToDelete)
         {
-            currentLocation = currentLocation->GetNext();
-            delete previous;
+            Node *temp = currentLocation;
+            currentLocation = currentLocation->GetNext(); // If list contains one node or more than one node. This line handles both cases.
+            delete temp;
             return;
         }
 
-        // Case 2.2: If node to be deleted is not first node
-        while(nodeToDelete != nullptr)
+        // Case 2.1: If there is only one node and it does not contains the required data
+        if (currentLocation->GetNext() == nullptr)
+        {
+            cout << "Data not found.\n";
+            return;
+        }
+
+        // Case 3: If there are more than one nodes
+        Node *nodeToDelete = currentLocation->GetNext();
+        Node *previous = currentLocation;
+
+        // Case 3.1: If node to delete is some middle node. The loop breaks when nodeToDelete reaches last node. It does not check whether last node contains required data or not.
+        while (nodeToDelete->GetNext() != nullptr)
         {
             if (nodeToDelete->GetData() == dataToDelete)
             {
                 previous->SetNext(nodeToDelete->GetNext());
                 delete nodeToDelete;
-                break;
+                return;
             }
             nodeToDelete = nodeToDelete->GetNext();
             previous = previous->GetNext();
         }
 
-        // Case 3: If data is not found
-        if (nodeToDelete == nullptr)
-        cout << "Data not found.\n";
+        // Case 3.2: If the node to be deleted is last node
+        if (nodeToDelete->GetData() == dataToDelete)
+        {
+            previous->SetNext(nullptr);
+            delete nodeToDelete;
+            return;
+        }
+        else // Case 4: If data is not found after traversing whole list
+            cout << "Data not found.\n";
     }
 
     ~List() // Destructor to free memory just incase user forgot to free memory
@@ -143,18 +157,21 @@ int main()
 
     myList.Insert(1);
 
-    myList.Delete(2); // Testing Case 3
+    myList.Delete(2); // Testing Case 4
 
-    myList.Delete(1); // Testing Case 2.1
+    myList.Delete(1); // Testing Case 2. If list contains only one node
 
     myList.Insert(2);
     myList.Insert(4);
+    myList.Insert(5);
 
-    myList.Print();
+    myList.Delete(2);  // Testing Case 2. If list contains more than one node
 
-    myList.Delete(4); // Testing Case 2.2
+    myList.Insert(6);
 
-    myList.Print();
-
+    myList.Delete(7); // Testing Case 4
+    myList.Delete(5); // Testing Case 3.1
+    myList.Delete(6); // Testing Case 3.2
+    myList.Delete(4);
     return 0;
 }
