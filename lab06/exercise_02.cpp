7b15b99725d7c05320549757b172435fb2c3280b
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-01-12 22:54:59 +0500

    Add InsertNode() and ~DoublyList() in DoublyList class in exercise 02 of lab 06

--- lab06/exercise_02.cpp
+++ lab06/exercise_02.cpp
@@ -57,7 +57,46 @@ public:
     {
         head = nullptr;
     }
-    void InsertNode(int);
+
+    void InsertNode(int data)
+    {
+        Node *newNode = new Node();
+        newNode->SetData(data);
+
+        // Case 1: If list has no node
+        if (head == nullptr)
+        {
+            head = newNode;
+            return;
+        }
+
+        // Case 2: If list has already some nodes
+        Node *current = head;
+        
+        while(current->GetNext() != nullptr)
+        {
+            current = current->GetNext();
+        }
+
+        current->SetNext(newNode);
+        newNode->SetPrevious(current);
+    }
+    
     void DeleteNode(int);
     void PrintList();
+
+    ~DoublyList() // Destructor to free memory just incase user forgot to free memory
+    {
+        if (head != nullptr)
+        {
+            Node *current = head;
+            Node *next;
+            while (current != nullptr)
+            {
+                next = current->GetNext(); // Traversing to second node before deleting the first node
+                delete current;
+                current = next;
+            }
+        }
+    }
 };
