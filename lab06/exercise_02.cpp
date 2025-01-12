494467d574684ee4a6c9be7be5a52237d07f65fa
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-01-12 23:42:35 +0500

    Add DeleteNode() in DoublyList class in exercise 02 in lab 06

--- lab06/exercise_02.cpp
+++ lab06/exercise_02.cpp
@@ -72,8 +72,8 @@ public:
 
         // Case 2: If list has already some nodes
         Node *current = head;
-        
-        while(current->GetNext() != nullptr)
+
+        while (current->GetNext() != nullptr)
         {
             current = current->GetNext();
         }
@@ -81,8 +81,66 @@ public:
         current->SetNext(newNode);
         newNode->SetPrevious(current);
     }
-    
-    void DeleteNode(int);
+
+    void DeleteNode(int dataToDelete)
+    {
+        if (head == nullptr)
+        {
+            cout << "Can't delete from an empty list.\n";
+            return;
+        }
+
+        // Case 1: If first node has to be deleted
+        if (head->GetData() == dataToDelete)
+        {
+            Node *temp = head;
+            head = head->GetNext();
+
+            // Case 1.1: If list has only one node. Then head's previous pointer cannot be updated. head's next pointer will already be set to nullptr.
+            if (head == nullptr)
+            {
+                delete temp;
+                return;
+            }
+            // Case 1.2: If list has more than one node. Then head's previous pointer should be set to nullptr. Since there is no node behind head.
+            else
+            {
+                head->SetPrevious(nullptr);
+                delete temp;
+                return;
+            }
+        }
+
+        // Case 2: If node to be deleted is not first node.
+        Node *current = head->GetNext(); // Since first node has already been checked.
+
+        while (current != nullptr)
+        {
+            if (current->GetData() == dataToDelete)
+            {
+                current->GetPrevious()->SetNext(current->GetNext()); // Setting the previous node of current node to point to the next node of current node. Since current node has to be deleted
+
+                // Case 2.1: If there are some nodes after current
+                if (current->GetNext() != nullptr)
+                {
+                    current->GetNext()->SetPrevious(current->GetPrevious()); // Setting the previous of current's next node to point to the current's previous node. Since current has to be deleted
+                    delete current;
+                    return;
+                }
+                // Case 2.2: If there is not any node after current.
+                else
+                {
+                    delete current;
+                    return;
+                }
+            }
+            current = current->GetNext();
+        }
+
+        // Case 3:
+        cout << "No node found with given data.\n";
+    }
+
     void PrintList();
 
     ~DoublyList() // Destructor to free memory just incase user forgot to free memory
@@ -100,3 +158,30 @@ public:
         }
     }
 };
+
+int main ()
+{
+    DoublyList list;
+
+    list.InsertNode(1);
+
+    list.DeleteNode(1); // Testing Case 1.1
+
+    list.InsertNode(1);
+    list.InsertNode(2); 
+
+    list.DeleteNode(1); // Testing Case 1.2
+
+    list.InsertNode(3);
+    list.InsertNode(4);
+    list.InsertNode(5);
+    list.InsertNode(6);
+
+    list.DeleteNode(5); // Testing Case 2.1
+
+    list.DeleteNode(6); // Testing Case 2.2
+
+    list.DeleteNode(10); // Testing Case 3
+    
+    return 0;
+}
