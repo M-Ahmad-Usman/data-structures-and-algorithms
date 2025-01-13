4df6e9504d2be689bd168d602997dc728407ea3e
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-01-13 18:23:11 +0500

    Solve exercise 02 of lab 07

---
+++ lab07/exercise_02.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+// Stack implementation using array
+int const STACKSIZE = 5;
+
+class Stack
+{
+    int stackArray[STACKSIZE];
+    int top;
+
+public:
+
+    Stack()
+    {
+        top = -1;
+    }
+
+    bool IsEmpty()
+    {
+        return (top == -1)? true: false;
+    }
+
+    bool IsFull()
+    {
+        return (top == STACKSIZE-1)? true: false;
+    }
+
+    int Size()
+    {
+        return STACKSIZE;
+    }
+
+    void Push(int data)
+    {
+        if (IsFull())
+        {
+            cout << "Stack is full. Can't Insert more items.\n";
+            return;
+        }
+        stackArray[++top] = data;
+    }
+
+    int Pop()
+    {
+        if (!IsEmpty())
+        {
+            return stackArray[top--];
+        }
+
+        throw runtime_error("Stack is empty. Can't pop from an empty stack.\n");
+    }
+
+    void Display()
+    {
+        if (IsEmpty())
+        {
+            cout << "Stack is empty. Can't display anything.\n";
+            return;
+        }
+
+        int counter = top;
+        while (counter >= 0)
+        {
+            cout << stackArray[counter--] << " ";
+        }
+        cout << "\n";
+    }
+
+};
+
+int main()
+{
+    Stack stk; 
+
+    stk.Push(100);  
+    stk.Push(200); 
+    stk.Push(300); 
+    stk.Push(400); 
+    stk.Push(500);
+
+    stk.Display(); 
+ 
+    cout << "\nPoping..\n"; 
+    stk.Pop(); 
+
+    stk.Display(); 
+    
+    system("pause");
+
+    return 0;
+}
