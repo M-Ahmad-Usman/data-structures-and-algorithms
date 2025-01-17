657d682fa915968f846dcbf7dfc5f13e9de94fa7
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-01-17 19:11:34 +0500

    Edit the Stack class to initialize stack with size given at runtime

--- lab07/exercise_03.cpp
+++ lab07/exercise_03.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
 using namespace std;
 
-// Stack implementation using array
-int const STACKSIZE = 5;
+// Stack implementation using array of dynamic size
 
 class Stack
 {
-    int stackArray[STACKSIZE];
+    int *arrptr;
+    int size;
     int top;
 
 public:
 
-    Stack()
+    Stack(int size)
     {
+        if (size <= 0)
+        {
+            throw runtime_error("Stack size must be a positive integer.\n");
+        }
+
         top = -1;
+        this->size = size;
+
+        arrptr = new int[size];
     }
 
     bool IsEmpty()
     {
-        return (top == -1)? true: false;
+        return (top == -1) ? true : false;
     }
 
     bool IsFull()
     {
-        return (top == STACKSIZE-1)? true: false;
+        return (top == size - 1) ? true : false;
     }
 
+    // Returns the current size of the stack (i.e items currently in stack)
     int Size()
     {
-        return STACKSIZE;
+        return top + 1;
     }
 
     void Push(int data)
@@ -38,14 +47,14 @@ public:
             cout << "Stack is full. Can't Insert more items.\n";
             return;
         }
-        stackArray[++top] = data;
+        arrptr[++top] = data;
     }
 
     int Pop()
     {
         if (!IsEmpty())
         {
-            return stackArray[top--];
+            return arrptr[top--];
         }
 
         throw runtime_error("Stack is empty. Can't pop from an empty stack.\n");
@@ -62,16 +71,20 @@ public:
         int counter = top;
         while (counter >= 0)
         {
-            cout << stackArray[counter--] << " ";
+            cout << arrptr[counter--] << " ";
         }
         cout << "\n";
     }
 
+    // Destructor to free the memory allocated to stack
+    ~Stack()
+    {
+        delete[] arrptr;
+    }
 };
 
 int main()
 {
-    
 
     return 0;
 }
