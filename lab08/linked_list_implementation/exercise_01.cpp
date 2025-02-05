d761d4391773edb2d236943ee7da5ea8150a80ab
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-05 23:33:23 +0500

    Implement LinkedQueue

--- lab08/linked_list_implementation/exercise_01.cpp
+++ lab08/linked_list_implementation/exercise_01.cpp
@@ -1,13 +1,5 @@
-#include <stdio.h>
+#include <iostream>
 using namespace std;
-
-template <typename T>
-class Node;
-
-int main()
-{
-    return 0;
-}
 
 template <typename T>
 class Node
@@ -18,17 +10,16 @@ private:
     Node *next;
 
 public:
-
     // Constructors
-    Node () { next = nullptr;}
+    Node() { next = nullptr; }
 
-    Node (T data)
+    Node(T data)
     {
         this->data = data;
         next = nullptr;
     }
 
-    Node (T data, Node *next)
+    Node(T data, Node *next)
     {
         this->data = data;
         this->next = next;
@@ -44,3 +35,132 @@ public:
 
     void SetNext(Node *next) { this->next = next; }
 };
+
+template <typename Q>
+class LinkedQueue
+{
+private:
+    Node<Q> *front;
+    Node<Q> *rear;
+
+    /* Queue visualization
+        front ------------ rear
+        1. Data is enqueued on "rear".
+        2. Data is dequeued from "front".
+    */
+
+public:
+    // Constructor
+    LinkedQueue()
+    {
+        front = nullptr;
+        rear = nullptr;
+    }
+
+    void Enqueue(Q data)
+    {
+        Node<Q> *newNode = new Node(data);
+
+        if (IsEmpty())
+        {
+            front = newNode;
+            rear = newNode;
+        }
+        else
+        {
+            rear->SetNext(newNode);
+            rear = rear->GetNext();
+        }
+    }
+
+    Q Dequeue()
+    {
+        // Case 1: If Queue is empty
+        if (IsEmpty())
+        {
+            throw runtime_error("Queue is empty. Can't Dequeue.");
+        }
+
+        Q data = front->GetData();
+
+        // Case 2: If Queue has only one node
+        if (front == rear)
+        {
+            delete rear;
+
+            front = nullptr;
+            rear = nullptr;
+
+            return data;
+        }
+
+        // Case 3: If Queue has more than one node
+        Node<Q> *tmp = front;
+
+        front = front->GetNext();
+
+        delete tmp;
+
+        return data;
+    }
+
+    void PrintQueue()
+    {
+        if (IsEmpty())
+            return;
+
+        Node<Q> *current = front;
+
+        while (current->GetNext() != nullptr)
+        {
+            cout << current->GetData() << "->";
+            current = current->GetNext();
+        }
+
+        cout << current->GetData() << "\n";
+    }
+
+    bool IsEmpty() { return (front == nullptr && rear == nullptr); }
+
+    Q GetFront()
+    {
+        if (IsEmpty())
+            throw runtime_error("Queue is empty. Can't get front.");
+
+        else
+            return front->GetData();
+    }
+
+    ~LinkedQueue()
+    {
+        Node<Q> *tmp = front;
+        Node<Q> *next;
+
+        while (tmp != nullptr)
+        {
+            next = tmp->GetNext();
+            delete tmp;
+            tmp = next;
+        }
+    }
+};
+
+int main()
+{
+    LinkedQueue<int> queue;
+
+    queue.Enqueue(10);
+    queue.Enqueue(20);
+    queue.Enqueue(30);
+
+    cout << "Queue: ";
+    queue.PrintQueue();
+
+    queue.Dequeue();
+    queue.Dequeue();
+    queue.Dequeue();
+
+    cout << "Queue: ";
+    queue.PrintQueue();
+    return 0;
+}
