bf14696039e34ba48228115593b5bd6c643b5bab
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-06 21:26:54 +0500

    Complete exercise 02 in both linked list and array implementation

---
+++ lab08/array_implementation/exercise_02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "exercise_01.cpp"
+using namespace std;
+
+int main ()
+{
+    ArrayQueue<int> lst(5); 
+    lst.PrintQueue(); 
+     
+    lst.Enqueue(100); 
+    lst.PrintQueue(); 
+     
+    lst.Enqueue(200); 
+    lst.PrintQueue(); 
+     
+    lst.Enqueue(300); 
+    lst.PrintQueue(); 
+     
+    lst.Enqueue(400); 
+    lst.PrintQueue(); 
+
+    cout << "The Fornt element is " << lst.GetFront() << "\n\n";
+
+    return 0;
+}
