3113250ce518a1b2ce9d9f57c3dd788f8cd4dd36
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-16 14:19:28 +0500

    Complete Exercise 02

---
+++ lab09/exercise_02.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <limits>
+#include <string.h>
+
+using namespace std;
+
+// Returns factorial of given positive integer
+unsigned long long CalcFactorial(int number)
+{
+    // Edge case
+    if (number < 0) 
+        throw runtime_error("Factorial of negative integers is not allowed.");
+
+    if (number == 1)
+        return 1;
+    else
+    {
+        return number * CalcFactorial(number - 1);
+    }
+}
+
+int TakeIntegerInput()
+{
+    string inputStr;
+    int number;
+
+    // Stores the index where non-integer character starts
+    size_t charPosition;
+
+    while(true)
+    {
+        try
+        {
+            getline(cin, inputStr);
+            number = stoi(inputStr, &charPosition);
+            
+            if (charPosition != inputStr.length())
+                throw runtime_error("Input contains non-integer characters");
+            else
+                return number;
+        }
+        catch(const exception& e)
+        {
+            cout << "Invalid input. Enter a positive integer without spaces: ";
+        }
+        
+    }
+}
+
+int main ()
+{
+    int number;
+    char choice;
+
+    do
+    {
+        cout << "Enter number: ";
+        number = TakeIntegerInput();
+        
+        try
+        {
+            unsigned long long result = CalcFactorial(number);
+            cout << "Factorial: " << result << "\n";
+        }
+        catch(const exception& e)
+        {
+            cerr << e.what() << '\n';
+        }
+
+        cout << "Do you want to calculate again? (Y/N) ";
+        cin >> choice;
+
+        // Clearing the input buffer before taking new input in getline
+        cin.ignore();
+
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
+}
